e4787fa386a4568da834cb13434e741fd1822573
Yanjun Chen <tmml1770998584@163.com>
2026-05-02 01:16:05 +0800

    Initial Commit

---
+++ WildMushroomHandbook/WildMushroomHandbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  WildMushroomHandbookApp.swift
+//  WildMushroomHandbook
+//
+//  Created by Zhuanz on 2026/5/2.
+//
+
+import SwiftUI
+
+@main
+struct WildMushroomHandbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
